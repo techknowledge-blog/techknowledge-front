f617d0da6f6a212ea2c9be6e5bcd4f4a57d9a455
Leticia Dias <contatoleticiadia@gmail.com>
2024-05-02 20:39:42 -0300

    refactor(app): removed markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,9 @@
 import React from "react";
-import Markdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
-import markdownStyle from "./markdown-styles.module.css";
 
 import "./App.css";
 
 function App() {
-  const markdown = `
-
-  # Testing markdown into react application
-
-  ## This is a subheading
-
-  Right now I just wanna know how it works
-
-  - This is a list item
-  - This is another list item
-
-  Right below this line is a code block
-
-  ~~~js
-
-  const randomFunction = () => {
-    const randomVariable = "Hello World";
-
-    return randomVariable;
-  }
-
-  console.log(randomFunction());
-  
-  ~~~
-
-  Now what comes up next is an image:
-
-  ![Image from when I won WorldSkills Americas](https://s2-g1.glbimg.com/YYxHxTw2ZxlZcbYg-aQItSGbh5A=/0x0:1024x768/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/1/V/kDPoAgTDiM6N7ANfNW1Q/worldskliss-3-1-.jpeg "Random Image sksksksksk").
-
-
-  `;
-
-  return (
-    <div>
-      <Markdown
-        children={markdown}
-        className={markdownStyle.reactMarkdown}
-        components={{
-          code(props) {
-            const { children, className, ...rest } = props;
-            const match = /language-(\w+)/.exec(className || "");
-            return match ? (
-              <SyntaxHighlighter
-                //{...rest}
-                PreTag="div"
-                children={String(children).replace(/\n$/, "")}
-                language={match[1]}
-                style={dracula}
-              />
-            ) : (
-              <code {...rest} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      />
-      <h1 className="text-3xl font-bold underline text-purple-700">
-        Hello world!
-      </h1>
-    </div>
-  );
+  return <p>Bem-vindo ao Techknowledge</p>;
 }
 
 export default App;
