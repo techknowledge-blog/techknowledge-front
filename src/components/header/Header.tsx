025561faec535c2401633a6e8ca53476ffd1ab35
Leticia Dias <contatoleticiadia@gmail.com>
2024-07-08 17:41:26 -0300

    feat: button light mode added to Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { Navbar } from "../navbar/Navbar";
 
+import buttonLightMode from "../../assets/icons/buttonLightMode.svg";
+import LightIcon from "../../assets/icons/light-icon-mode.svg";
 import techknowledgeLogo from "../../assets/images/techknowledge-logo.png";
 
 export function Header() {
   let options = ["Home", "Sobre", "Posts", "Comunidade"];
 
   return (
-    <header className="md:flex md:px-10 items-center justify-between gap-32">
+    <header className="md:flex items-center justify-between gap-14">
       <img
         src={techknowledgeLogo}
         alt="Está escrito o nome Techknowledge na cor branca."
       />
 
       <Navbar navbarOptions={options} />
+
+      <div className="dark-light-mode md:flex md:items-center gap-2 hidden md:visible">
+        <img
+          src={buttonLightMode}
+          alt="Botão para ativar o modo claro e escuro da página"
+        />
+        <img src={LightIcon} alt="Icone no formato de um sol." />
+      </div>
     </header>
   );
 }
