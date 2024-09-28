3ec13e28efb1eb19f053be07400f74291bc8aefa
Leticia Dias <contatoleticiadia@gmail.com>
2024-09-28 16:30:41 -0300

    feat: tweaks to the spacing and font size in the header.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
   let options = ["Sobre", "Posts", "Projetos", "Comunidade"];
 
   return (
-    <header className="md:flex items-center justify-between gap-14">
+    <header className="md:flex items-center justify-evenly gap-4 pt-6 px-4">
       <img
         src={techknowledgeLogo}
         alt="Está escrito o nome Techknowledge na cor branca."
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -15,19 +15,21 @@ export function Navbar({ navbarOptions }: NavbarProps) {
     <div className={styles["primary-navigation"]}>
       <div
         onClick={() => setOpen(!open)}
-        className="absolute right-8 top-9 cursor-pointer md:hidden"
+        className="absolute right-8 top-5 cursor-pointer md:hidden"
       >
         <img src={open ? close : menu} alt="" />
       </div>
 
       <ul
-        className={`md:flex md:items-center gap-8 text-xl md:pb-0  
+        className={`md:flex md:items-center gap-4 text-xl md:pb-0  
       md:static md:z-auto md:w-auto md:pl-0 transition-all 
       duration-500 ease-in-out ${open ? "visible" : "hidden"}`}
       >
         {navbarOptions.map((options) => (
           <li key={options} className="md:ml-2 md:my-0 my-7">
-            <a href="#">{options}</a>
+            <a href="#" className="text-base">
+              {options}
+            </a>
           </li>
         ))}
       </ul>
