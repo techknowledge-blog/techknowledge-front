be081afbc9da53938b567915513113172a89a936
Leticia Dias <contatoleticiadia@gmail.com>
2024-11-08 22:43:25 -0300

    refactor: removed sections and added the SectionMain and SectionAbout components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,18 @@
 import React from "react";
 
 import { Header } from "./components/header/Header";
-import styles from "./App.module.css";
-
-import ImageTecnologies from "./assets/images/technologies.png";
-import { CaretRight } from "../node_modules/@phosphor-icons/react/dist/index";
+import { SectionAbout } from "./sections/SectionAbout";
+import { SectionMain } from "./sections/SectionMain";
 
 export function App() {
   return (
-    <div>
+    <div className="flex flex-col mx-auto p-4 max-w-5xl">
       <Header />
 
-      <div className={styles.wrapper}>
-        <main>
-          <section className={styles.section_main}>
-            <div className={styles.column_left}>
-              <p className="text-5xl w-[34rem] font-semibold leading-[3.5rem]">
-                Um blog para compartilharmos conhecimento da área da{" "}
-                <span className="text-blue-500">tecnologia</span>.
-              </p>
-
-              <div className={styles.buttons}>
-                <a
-                  href="#"
-                  className="bg-blue-500 py-3 px-6 flex items-center gap-1
-                  justify-center rounded-full font-semibold hover:bg-blue-600"
-                >
-                  Sobre nós
-                  <CaretRight size={16} color="#ffffff" weight="bold" />
-                </a>
-                <a
-                  href="#"
-                  className="text-blue-500 py-3 px-6 flex items-center 
-                  justify-center gap-1 font-semibold hover:underline"
-                >
-                  Comunidade
-                  <CaretRight size={16} color="#39A0ED" weight="bold" />
-                </a>
-              </div>
-            </div>
-
-            <div>
-              <img src={ImageTecnologies} className="h-96" />
-            </div>
-          </section>
-        </main>
-      </div>
+      <main className="flex flex-col items-center">
+        <SectionMain />
+        <SectionAbout />
+      </main>
     </div>
   );
 }
