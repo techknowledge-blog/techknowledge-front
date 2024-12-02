7dd7d3a2bddbd3f466bd22ce1ef035aa4e208037
Leticia Dias <contatoleticiadia@gmail.com>
2024-12-01 22:31:22 -0300

    feat: created all the application routes.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,13 +3,18 @@ import styles from "./Navbar.module.css";
 
 import close from "../../assets/icons/close.svg";
 import menu from "../../assets/icons/menu.svg";
+import { Link } from "../../../node_modules/react-router/dist/production/index";
 
-interface NavbarProps {
-  navbarOptions: string[];
-}
+export function Navbar() {
+  const [open, setOpen] = useState(false);
 
-export function Navbar({ navbarOptions }: NavbarProps) {
-  let [open, setOpen] = useState(false);
+  const routes = [
+    { path: "/", label: "" },
+    { path: "/about", label: "Sobre" },
+    { path: "/posts", label: "Posts" },
+    { path: "/projects", label: "Projetos" },
+    { path: "/community", label: "Comunidade" },
+  ];
 
   return (
     <div className={styles["primary-navigation"]}>
@@ -17,19 +22,18 @@ export function Navbar({ navbarOptions }: NavbarProps) {
         onClick={() => setOpen(!open)}
         className="absolute right-8 top-7 cursor-pointer md:hidden"
       >
-        <img src={open ? close : menu} alt="" />
+        <img src={open ? close : menu} alt="Menu" />
       </div>
-
       <ul
         className={`md:flex md:items-center gap-4 text-xl md:pb-0  
       md:static md:z-auto md:w-auto md:pl-0 transition-all 
       duration-500 ease-in-out ${open ? "visible" : "hidden"}`}
       >
-        {navbarOptions.map((options) => (
-          <li key={options} className="md:ml-2 md:my-0 my-7">
-            <a href="#" className="text-base">
-              {options}
-            </a>
+        {routes.map(({ path, label }) => (
+          <li key={path} className="md:ml-2 md:my-0 my-7">
+            <Link to={path} className="text-base">
+              {label}
+            </Link>
           </li>
         ))}
       </ul>
