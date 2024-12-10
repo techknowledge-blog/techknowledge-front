61740b1ac5263ac86c517aaa7ff51ae0b3f81ad4
Leticia Dias <contatoleticiadia@gmail.com>
2024-12-10 10:18:48 -0300

    feat: added Tailwind configurations to enable Markdown support.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx, mdx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -22,5 +22,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
