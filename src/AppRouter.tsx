2c1de330d8afce8970b56e2f1a5153bfb684dee8
Leticia Dias <contatoleticiadia@gmail.com>
2024-12-10 10:16:21 -0300

    feat: route created for the post.

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,6 +3,7 @@ import {
   Route,
   Routes,
 } from "../node_modules/react-router/dist/production/index";
+import { Post } from "./components/posts/post/Post";
 import { Home } from "./pages/Home";
 import { Posts } from "./pages/Posts";
 
@@ -11,6 +12,7 @@ export function AppRouter() {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/posts" element={<Posts />}></Route>
+      <Route path="/post/:id" element={<Post />}></Route>
     </Routes>
   );
 }
