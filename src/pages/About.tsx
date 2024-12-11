c8ab60ecaf535053b13b1af532cf79a1c93a5158
Leticia Dias <contatoleticiadia@gmail.com>
2024-12-11 20:12:53 -0300

    feat: created the about and community pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function About() {
+  return <p className="mt-8 font-bold">Página Sobre em construção</p>;
+}
---
+++ src/pages/Community.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Community() {
+  return <p className="font-bold mt-8">Página Comunidade em construção</p>;
+}
