b90bea6da8047c78f0c57ceeaf9d87b60690c633
Leticia Dias <contatoleticiadia@gmail.com>
2024-12-11 20:11:20 -0300

    feat: created the route for about and community

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,6 +4,8 @@ import {
   Routes,
 } from "../node_modules/react-router/dist/production/index";
 import { Post } from "./components/posts/post/Post";
+import { About } from "./pages/About";
+import { Community } from "./pages/Community";
 import { Home } from "./pages/Home";
 import { Posts } from "./pages/Posts";
 
@@ -11,8 +13,10 @@ export function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
       <Route path="/posts" element={<Posts />}></Route>
       <Route path="/post/:id" element={<Post />}></Route>
+      <Route path="/community" element={<Community />}></Route>
     </Routes>
   );
 }
