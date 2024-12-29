52a1815b67633b547c5cac3038da81a81b70dd2e
Mikkaiser <mikaelrsimoes19@gmail.com>
2024-12-29 22:46:05 +0400

    feat: add dockerfile for development environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import mdx from "@mdx-js/rollup";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mdx()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 });
