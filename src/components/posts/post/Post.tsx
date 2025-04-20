918d7323bef54f9210dad574f4814058da6d4064
Leticia Dias <contatoleticiadia@gmail.com>
2025-04-20 11:15:28 -0300

    feat: added author info and publish date on post page

--- src/components/posts/post/Post.tsx
+++ src/components/posts/post/Post.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { getPostBySlug } from "../../../services/postsService";
 import ReactMarkdown from "react-markdown";
+import { formatDateToCustomFormat } from "../../../utils/formatDate";
 
+import ImageLeticiaAuthor from "../../../assets/images/leticia.jpeg";
+import ImageMikaelAuthor from "../../../assets/images/mikael.jpeg";
 interface PostProps {
   id: number;
   title: string;
@@ -34,15 +37,34 @@ export function Post() {
   }
 
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
+    <div className="px-4 sm:px-6 lg:px-8 mt-12">
+      <div className="flex items-center">
+        <div className="h-6 w-1 dark:bg-blue-300 bg-blue-500 mr-4"></div>
+        <p className="font-bold text-sm sm:text-base dark:text-white text-[#62748E]">
+          {formatDateToCustomFormat(post.createdAt)}
+        </p>
+      </div>
+
       <div className="prose prose-invert max-w-none mt-8">
-        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold dark:text-white text-black">
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold dark:text-white text-black mb-0">
           {post.title}
         </h1>
-        <p className="font-bold text-sm sm:text-base dark:text-white text-[#62748E]">
-          escrito por: {post.author.name}
-        </p>
-        <div className="mt-6">
+
+        <div className="flex items-center gap-2">
+          <img
+            src={
+              post.author.name === "Leticia Dias"
+                ? ImageLeticiaAuthor
+                : ImageMikaelAuthor
+            }
+            className="w-10 h-10 border-2 border-solid dark:border-blue-300 border-blue-500 rounded-full object-cover"
+          />
+          <p className="font-bold text-sm sm:text-base dark:text-white text-[#62748E]">
+            escrito por: {post.author.name}
+          </p>
+        </div>
+
+        <div className="mt-2">
           <ReactMarkdown className="prose dark:prose-invert prose-h1:text-black dark:prose-h1:text-white max-w-none">
             {post.content}
           </ReactMarkdown>
