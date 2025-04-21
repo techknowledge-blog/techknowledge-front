fbdc9cc3437d683cf4157c1223a8096cde695efd
Leticia Dias <contatoleticiadia@gmail.com>
2025-04-21 16:20:06 -0300

    refactor: remove icons and update text

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,4 @@
 import React, { useContext } from "react";
-import {
-  GithubLogo,
-  InstagramLogo,
-  LinkedinLogo,
-  XLogo,
-} from "@phosphor-icons/react";
 
 import TechknowledgeMiniLogo from "../../assets/images/techKnowledge-mini-logo.svg";
 import TechknowledgeMiniLogoLight from "../../assets/images/techknowledge-minilogo-light.png";
@@ -14,22 +8,10 @@ export function Footer() {
   const { darkMode } = useContext(ThemeContext);
 
   return (
-    <footer className="flex flex-col items-center justify-between gap-6 border-b-4 dark:border-b-[#00C2FF] border-b-blue-500 mt-16 p-4 sm:flex-row sm:py-6 lg:py-8">
+    <footer className="flex flex-col items-center justify-between gap-6 border-b-4 dark:border-b-[#00C2FF] border-b-blue-500 mt-16 py-4 px-2 sm:flex-row sm:py-6 lg:py-8">
       <div className="flex flex-col items-center sm:flex-row sm:gap-4">
-        <div className="flex gap-6 mb-4 sm:mb-0">
-          <InstagramLogo size={24} />
-
-          <XLogo size={24} />
-
-          <LinkedinLogo size={24} />
-
-          <GithubLogo size={24} />
-        </div>
-
-        <div className="hidden sm:block sm:mx-4 sm:h-6 sm:w-1 dark:bg-blue-300 bg-blue-500"></div>
-
         <p className="text-center sm:text-left">
-          © 2022. Todos os direitos reservados ao TechKnowledge.
+          © 2025. Todos os direitos reservados ao TechKnowledge.
         </p>
       </div>
 
