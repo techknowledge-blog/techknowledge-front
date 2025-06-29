ca3a5660d49d30600f9a28dbf40a803189773b8c
Leticia Dias <contatoleticiadia@gmail.com>
2025-06-29 14:17:27 -0300

    feat: community section removed from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { SectionAbout } from "../sections/SectionAbout";
-import { SectionCommunity } from "../sections/SectionCommunity";
 import { SectionEvents } from "../sections/SectionEvents";
 import { SectionLatestPosts } from "../sections/SectionLatestPosts";
 import { SectionMain } from "../sections/SectionMain";
@@ -15,7 +14,6 @@ export function Home() {
       <SectionAbout />
       <SectionLatestPosts />
       <SectionEvents />
-      <SectionCommunity />
     </>
   );
 }
