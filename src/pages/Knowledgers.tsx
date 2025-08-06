186dfb0ba63a66f9a6638659454f1b58097164e8
Leticia Dias <contatoleticiadia@gmail.com>
2025-08-06 20:39:09 -0300

    feat: knowledgers page created

--- src/pages/Knowledgers.tsx
+++ src/pages/Knowledgers.tsx
@@ -1,58 +1,21 @@
-import React, { useContext } from "react";
+import React from "react";
 
-import ImageAstronautDark from "../assets/images/astronaut.png";
-import ImageAstrounautLight from "../assets/images/astronaut-lightmode.png";
-
-import { ThemeContext } from "../context/ThemeContext";
+import { CardKnowledger } from "../components/cardknowledger/CardKnowledger";
 
 export function Knowledgers() {
-  const { darkMode } = useContext(ThemeContext);
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen lg:w-[60rem] px-4">
-      <div className="flex items-center justify-center lg:justify-start w-full mt-10 md:mt-0">
-        <p className="bg-[#93C5FD] w-[16rem] h-[2.5rem] rounded-full mb-8 flex items-center justify-center text-black font-bold text-lg">
-          #Em Breve 2025
+    <div className="px-4 screen-custom:w-[22rem] sm-medium:w-[24rem] md:w-[46rem] lg:w-[62rem]">
+      <div className="mt-16 flex flex-col items-center">
+        <h1 className="text-2xl font-bold text-center sm:text-3xl dark:text-white text-black">
+          Somos os Knowledgers
+        </h1>
+
+        <p className="text-center py-4 lg:w-[28rem]">
+          Conhecimento que transforma, vozes que inspiram.
         </p>
       </div>
 
-      <div className="flex flex-col items-center lg:flex-row lg:items-center lg:justify-between w-full max-w-screen-lg">
-        <div className="text-center lg:text-left lg:mr-8 flex flex-col items-center lg:items-start">
-          <h2 className="text-xl font-bold mb-8 dark:bg-gradient-to-r dark:from-white dark:to-blue-300 dark:bg-clip-text dark:text-transparent text-blue-500 md:text-2xl lg:text-4xl">
-            Aguardem Novidades!
-          </h2>
-
-          <div>
-            <p className="text-base font-semibold mb-4 max-w-md md:text-lg lg:text-2xl lg:max-w-[573px] dark:text-white text-[#62748E]">
-              Estamos trabalhando para lançar nossa página de Comunidade no
-              próximo ano.
-            </p>
-
-            <p className="text-base font-semibold max-w-md md:text-lg lg:text-2xl lg:max-w-[573px] dark:text-white text-[#62748E]">
-              Um espaço feito para troca de ideias, networking e colaboração
-              entre apaixonados por tecnologia. Fique de olho 👀 – coisas
-              incríveis estão a caminho!
-            </p>
-
-            <p className="mt-8 font-bold dark:text-blue-300 text-blue-500">
-              #community #techknowledge #networking
-            </p>
-          </div>
-        </div>
-        {darkMode ? (
-          <img
-            src={ImageAstronautDark}
-            alt="Astronauta"
-            className="mt-8 object-cover h-[200px] md:h-[300px] lg:mt-0 lg:h-[380px]"
-          />
-        ) : (
-          <img
-            src={ImageAstrounautLight}
-            alt="Astronauta"
-            className="mt-8 object-cover h-[200px] md:h-[300px] lg:mt-0 lg:h-[380px]"
-          />
-        )}
-      </div>
+      <CardKnowledger />
     </div>
   );
 }
