566267612fc6648720fcebcdafe69d449f120422
Leticia Dias <contatoleticiadia@gmail.com>
2025-10-27 18:56:39 -0300

    feat(header): added responsive menu modal with transition animation

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,12 +1,9 @@
 import React, { useContext, useState } from "react";
-
-import CloseDark from "../../../assets/icons/close.svg";
-import MenuHamburgerDark from "../../../assets/icons/menu.svg";
-import CloseLight from "../../../assets/icons/close-light.svg";
-import MenuHamburgerLight from "../../../assets/images/menu-hamburger-lightmode.png";
-
 import { Link } from "react-router-dom";
+import { List } from "@phosphor-icons/react";
+
 import { ThemeContext } from "../../../context/ThemeContext";
+import { MenuModal } from "../../ui/menuModal/menuModal";
 
 export function Navbar() {
   const [open, setOpen] = useState(false);
@@ -21,28 +18,33 @@ export function Navbar() {
   ];
 
   return (
-    <div className="pl-3 md:pl-0">
-      <div
+    <nav>
+      <button
         onClick={() => setOpen(!open)}
-        className="absolute right-4 top-7 cursor-pointer md:hidden"
+        className="absolute right-4 top-8 cursor-pointer md:hidden"
       >
         {darkMode ? (
-          <img src={open ? CloseDark : MenuHamburgerDark} alt="Menu" />
+          <span className="inline-flex items-center justify-center w-12 h-9 border border-white/10 rounded-full">
+            <List
+              size={24}
+              color="#ffffff"
+              className={`transition-all duration-300 ${
+                open
+                  ? "opacity-0 rotate-90 scale-75"
+                  : "opacity-100 rotate-0 scale-100"
+              }`}
+            />
+          </span>
         ) : (
-          <img
-            src={open ? CloseLight : MenuHamburgerLight}
-            alt="Menu"
-            className="object-cover"
-          />
+          <span className="inline-flex items-center justify-center w-12 h-9 border border-black/10 rounded-full">
+            <List size={24} color="#000000" />
+          </span>
         )}
-      </div>
-      <ul
-        className={`md:flex md:items-center gap-4 text-xl md:pb-0  
-  md:static md:z-auto md:w-auto md:pl-0 transition-all 
-  duration-500 ease-in-out ${open ? "visible" : "hidden"}`}
-      >
+      </button>
+
+      <ul className="hidden md:flex md:items-center gap-4 text-xl">
         {routes.map(({ path, label }) => (
-          <li key={path} className="relative md:ml-2 md:my-0 my-7">
+          <li key={label} className="relative md:ml-2 md:my-0 my-7">
             <Link
               to={path}
               className="text-base relative after:content-[''] after:absolute after:left-0 after:bottom-[-4px] after:w-full after:h-[2px] dark:after:bg-blue-300 after:bg-blue-500 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
@@ -52,6 +54,8 @@ export function Navbar() {
           </li>
         ))}
       </ul>
-    </div>
+
+      <MenuModal isOpen={open} onClose={() => setOpen(false)} routes={routes} />
+    </nav>
   );
 }
---
+++ src/components/ui/menuModal/menuModal.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import TechknowledgeLightLogo from "../../../assets/images/new-logo-lightmode.svg";
+import { X } from "@phosphor-icons/react";
+import { Link } from "react-router";
+
+interface RouteProps {
+  path: string;
+  label: string;
+}
+
+interface MenuModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  routes: RouteProps[];
+}
+
+export function MenuModal({ isOpen, onClose, routes }: MenuModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black/30 z-40 flex items-center justify-center md:hidden">
+      <div className="relative bg-white w-[95%] h-[98vh] rounded-2xl py-5 px-2 shadow-lg border-2 border-white/10 flex flex-col justify-between animate-scaleUp">
+        <header className="flex items-center justify-between">
+          <img
+            src={TechknowledgeLightLogo}
+            className="h-[38px] object-cover lg:h-14"
+            alt=""
+          />
+
+          <button
+            onClick={onClose}
+            className="absolute right-2 top-5 cursor-pointer"
+            aria-label="Botão para fechar o menu"
+          >
+            <span className="inline-flex items-center justify-center w-12 h-9 border border-black/10 rounded-full">
+              <X size={24} color="#000000" />
+            </span>
+          </button>
+        </header>
+
+        <ul className="flex flex-col items-start gap-6 mt-6 flex-1">
+          {routes.map(({ path, label }) => (
+            <li key={label}>
+              <Link
+                to={path}
+                onClick={onClose}
+                className="text-xl text-black relative after:content-[''] after:absolute after:left-0 after:bottom-[-4px] after:w-full after:h-[2px] dark:after:bg-blue-300 after:bg-blue-500 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <footer>
+          <p className="text-lg text-black">
+            © Todos os direitos reservados ao Techknowledge.
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
+}
